62f7defb87f915973091c6c68ea79408fd58cc35
ayushrajput545 <rajputayush694@gmail.com>
2025-01-12 19:15:25 +0530

    Fixing change password api

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,8 +2,9 @@ const express= require('express');
 const router = express.Router();
 
 const {sendOTP} = require('../controllers/Auth');
-const{signUp,login}= require('../controllers/Auth');
+const{signUp,login , changePassword}= require('../controllers/Auth');
 const{resetPasswordToken , resetPassword} = require('../controllers/ResetPassword');
+const {auth} = require('../middlewares/auth')
 
 router.post('/sendOTP',sendOTP);
 router.post('/signup',signUp);
@@ -11,5 +12,7 @@ router.post('/login',login);
 
 router.post('/reset-password-token',resetPasswordToken);
 router.post('/reset-password',resetPassword); 
+
+router.post("/changepassword", auth, changePassword)
 
 module.exports = router
